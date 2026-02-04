0fef23626114366f1b1ff42067287f36cb2fe4e9
KunalSalunkhe12 <salunkhekunal594@gmail.com>
2026-02-04 13:34:49 +0530

    Add Haffer font files and integrate into layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,49 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
+
 import "./globals.css";
+
+const haffer = localFont({
+  src: [
+    {
+      path: "./fonts/Haffer/Haffer-TRIAL-Thin.woff2",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Haffer/Haffer-TRIAL-Light.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Haffer/Haffer-TRIAL-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Haffer/Haffer-TRIAL-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Haffer/Haffer-TRIAL-SemiBold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Haffer/Haffer-TRIAL-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Haffer/Haffer-TRIAL-Black.woff2",
+      weight: "900",
+      style: "normal",
+    },
+  ],
+  variable: "--font-haffer",
+});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +68,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${haffer.variable} antialiased`}
       >
         {children}
       </body>
